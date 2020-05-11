ad3dbdbac309929dbb5f5104c6dde931e53111ca
Ivan Bonkin <inbonkin@avito.ru>
2020-05-12 02:06:47 +0300

    Setup k8s with Kubespray

--- Vagrantfile
+++ Vagrantfile
@@ -3,18 +3,11 @@
 #
 #
 
-#Script for creating kvm (Kernel native) vm's for kubernetes
-#best be doing this on a fresh box lest things get cattywampus
-#need a bridge set up under br0
-
 #Net work prefix in which a single digit is appended
 #ex 192.168.1.5 will have a master at 192.168.1.50 and workers starting from 192.168.1.51
 NETWORK_PREFIX="10.0.10.9"
-
-#make sure libvirt, qemu, kvm etc are installed
-#make sure other hypervisors such as virtualbox are not
 ENV['VAGRANT_DEFAULT_PROVIDER'] = 'libvirt'
-IMAGE_NAME = "peru/ubuntu-18.04-server-amd64"
+IMAGE_NAME = "generic/ubuntu1804"
 
 #right now NUM_NODES must be under 9
 NUM_NODES = 2
@@ -22,11 +15,42 @@ NUM_NODES = 2
 Vagrant.configure("2") do |config|
     config.ssh.insert_key = false
 
+    config.vm.provision "shell" do |s|
+        ssh_prv_key = ""
+        ssh_pub_key = ""
+        if File.file?("#{Dir.home}/.ssh/id_rsa")
+            ssh_prv_key = File.read("#{Dir.home}/.ssh/id_rsa")
+            ssh_pub_key = File.readlines("#{Dir.home}/.ssh/id_rsa.pub").first.strip
+        else
+            puts "No SSH key found. You will need to remedy this before pushing to the repository."
+        end
+        s.inline = <<-SHELL
+            if grep -sq "#{ssh_pub_key}" /root/.ssh/authorized_keys; then
+                echo "SSH keys already provisioned."
+                exit 0;
+            fi
+            echo "SSH key provisioning."
+            mkdir -p /root/.ssh/
+            touch /root/.ssh/authorized_keys
+            echo #{ssh_pub_key} >> /root/.ssh/authorized_keys
+            echo #{ssh_pub_key} > /root/.ssh/id_rsa.pub
+            chmod 644 /root/.ssh/id_rsa.pub
+            echo "#{ssh_prv_key}" > /root/.ssh/id_rsa
+            chmod 600 /root/.ssh/id_rsa
+            chown -R root:root /root
+            echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+            usermod --expiredate 1 vagrant
+            systemctl restart ssh
+            exit 0
+        SHELL
+    end
+
     config.vm.provider :libvirt do |libvirt|
-        libvirt.memory = 1024
-        libvirt.cpus = 1
+        libvirt.memory = 4096
+        libvirt.cpus = 2
+        libvirt.cputopology :sockets => '1', :cores => '1', :threads => '2'
         libvirt.driver = "kvm"
-        libvirt.nested = false
+        libvirt.nested = true
         libvirt.management_network_name = 'node-network'
         libvirt.management_network_address = '10.0.10.0/24'
     end
--- kvm.sh
+++ kvm.sh
@@ -43,8 +43,13 @@ if [[ "$distro" == "opensuse-leap" ]]; then
                             bridge-utils \
                             vagrant \
                             ansible
+  # OpenSUSE bug workaround
+  mv /opt/vagrant/embedded/lib/libreadline.so.7{,.disabled}
   vagrant plugin install vagrant-libvirt
+  mv /opt/vagrant/embedded/lib/libreadline.so.7{.disabled,}
 fi
+
+cat /dev/zero | ssh-keygen -q -N ""
 
 echo "stdio_handler="\""file"\" >> /etc/libvirt/qemu.conf
 
@@ -52,8 +57,8 @@ systemctl start virtlogd
 
 systemctl start libvirtd
 
+rm -r /usr/share/vagrant-vms
 mkdir /usr/share/vagrant-vms
-cd /usr/share/vagrant-vms
 cp "$DIR/Vagrantfile" /usr/share/vagrant-vms
 
 virsh net-define "$DIR/vnet-definition.xml"
@@ -63,10 +68,43 @@ virsh net-list
 
 systemctl restart libvirtd
 
-export VAGRANT_DEFAULT_PROVIDER=libvirt
-vagrant up --provider=libvirt
+(cd /usr/share/vagrant-vms;\
+vagrant up --provider=libvirt;\
+vagrant status)
 
-vagrant status
+cd "$DIR/kubespray/"
+git clean -fdx
+git submodule update --init "$DIR/kubespray/"
+curr_branch=$(git rev-parse --abbrev-ref HEAD)
+tags=$(git tag -l --sort -version:refname | head -n 10 | awk '{print v++,$1}')
+tag=$(dialog --menu "Checkout another Kubespray version instead of ${curr_branch}?" 20 25 25 ${tags} 3>&2 2>&1 1>&3)
 
+if [[ -z ${tag} ]]; then
+  echo "Current revision will be used"
+else
+  arr=(${tags})
+  idx=$(( tag * 2 + 1 ))
+  git checkout "${arr[idx]}"
+fi
 
+pip3 install -r requirements.txt
 
+# When working with Kubespray, it is first advised to copy the default sample configuration from inventory/sample
+cp -rfp inventory/sample inventory/mycluster
+
+declare -a IPS=(10.0.10.90 10.0.10.91 10.0.10.92)
+CONFIG_FILE=inventory/mycluster/hosts.yml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
+
+# change calico -> cilium
+sed -i 's/^\(\s*kube_network_plugin\s*:\s*\).*/\1cilium/' inventory/mycluster/group_vars/k8s-cluster/k8s-cluster.yml
+
+# for ‘kubectl top nodes’ & ‘kubectl top pods’ commands to work with no authentication/authorization
+echo "kube_read_only_port: 10255" >> inventory/mycluster/group_vars/all/all.yml
+echo "bootstrap_os: ubuntu" >> inventory/mycluster/group_vars/all/all.yml
+echo "kubeconfig_localhost: true" >> inventory/mycluster/group_vars/k8s-cluster/k8s-cluster.yml
+echo "kubectl_localhost: true" >> inventory/mycluster/group_vars/k8s-cluster/k8s-cluster.yml
+
+ansible-playbook -i inventory/mycluster/hosts.yml cluster.yml -u root -b -v --private-key=/root/.ssh/id_rsa --timeout=60
+
+export KUBECONFIG=inventory/mycluster/artifacts/admin.conf
+kubectl get nodes
